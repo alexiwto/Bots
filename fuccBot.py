2ea4253414d587635a07e44fa885c304bfa360fc
Alejandro García <asionium@gmail.com>
2018-01-14 15:37:35 +0100

    Create fuccBot.py
    Bot that posts fucc + every single word in the english dictionary (460k+ words) in https://twitter.com/fuccword (every hour).
    
    Automated with crontab.

---
+++ fuccBot.py
@@ -0,0 +1,36 @@
+import tweepy
+import tweepy, time
+from tweepy import OAuthHandler
+from random import randint
+
+fuck = 'fucc '
+palabro = ''
+fuckpalabro = ''
+palabras = []
+
+consumer_key = ' '
+consumer_secret = ' '
+access_token = ' '
+access_secret = ' '
+
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+tweetBot = tweepy.API(auth)
+
+def palabraRandom():
+        with open('/root/python/palabrasIngles.txt', 'r', encoding='utf-8') as file:
+                for palabra in file:
+                        palabras.append(palabra)
+        file.close()
+        numeroRandom = randint(0, len(palabras)-1)
+        return palabras[numeroRandom]
+
+
+
+
+palabro = palabraRandom().replace("\n", "")
+fuckpalabro = (fuckpalabro.join([fuck,palabro]))
+
+
+tweetBot.update_status(fuckpalabro)
